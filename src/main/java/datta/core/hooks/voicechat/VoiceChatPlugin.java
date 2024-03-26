264ad6982f544cea591dd632e5120dc5eba6804b
Favi0ne <faviorivarola@hotmail.com>
2024-03-26 18:16:41 -0300

    update

--- src/main/java/datta/core/hooks/voicechat/VoiceChatPlugin.java
+++ src/main/java/datta/core/hooks/voicechat/VoiceChatPlugin.java
@@ -5,8 +5,11 @@ import de.maxhenkel.voicechat.api.VoicechatPlugin;
 import de.maxhenkel.voicechat.api.events.EventRegistration;
 import de.maxhenkel.voicechat.api.events.MicrophonePacketEvent;
 import org.bukkit.entity.Player;
+import org.bukkit.permissions.Permission;
+import org.bukkit.permissions.PermissionDefault;
 
 public class VoiceChatPlugin implements VoicechatPlugin {
+    public static Permission PERMISSION = new Permission("spreenstudios.voicechat", PermissionDefault.OP);
     @Override
     public String getPluginId() {
         return "spreenstudios-voicechat";
@@ -27,11 +30,20 @@ public class VoiceChatPlugin implements VoicechatPlugin {
         if (!(event.getSenderConnection().getPlayer().getPlayer() instanceof Player player))
             return;
 
+        player.sendMessage("0");
+
         boolean muted = false;
         if(player.hasMetadata("voicestick"))
             muted = player.getMetadata("voicestick").get(0).asBoolean();
 
-        if(muted && !player.hasPermission("spreenstudios.voicechat"))
+        player.sendMessage("1 -> "+muted);
+
+        if(muted && !player.hasPermission(PERMISSION)){
             event.cancel();
+            player.sendMessage("2 -> cancel");
+        }else{
+            player.sendMessage("3 -> no cancel");
+        }
+        player.sendMessage("muted: "+muted+" permission: "+player.hasPermission(PERMISSION));
     }
 }
